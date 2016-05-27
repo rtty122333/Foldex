adcddbdba6a46879f3354c0718b57c078b12bcfb
centos <centos@localhost.localdomain>
2016-05-27 10:57:07 +0000

    delete comment

--- test/port_forward.py
+++ test/port_forward.py
@@ -126,16 +126,3 @@ class ServerProxy(Singleton):
     def delete_proxy(self, port):
         self.server_list[port].stop()
         del self.server_list[port]
-
-
-#if __name__ == '__main__':
-#    try:
-#        proxy = ServerProxy()
-#        port1 = proxy.add_proxy('192.168.161.14', 3389)
-#        port2 = proxy.add_proxy('100.100.100.105', 22)
-#        print "proxy1: ", port1, "proxy2: ",port2
-#        time.sleep(60)
-#        proxy.delete_proxy(port1)
-#    except KeyboardInterrupt:
-#        print "Server stopped"
-#        sys.exit(1)
