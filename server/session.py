3e30b0bc97da549010cd3f563e1d6ebfdc85fdbf
solarsail <newleaf.lu@gmail.com>
2016-06-08 14:03:56 +0800

    迁移到 evercloud 接口（续）

--- server/session.py
+++ server/session.py
@@ -79,9 +79,12 @@ class Session(object):
 
         自动进行身份验证，验证失败时抛出异常。
         """
-        auth_url = 'http://{}:{}/login'.format(CONF.evercloud.host, CONF.evercloud.port)
+        self.host = 'http://{}:{}'.format(CONF.evercloud.host, CONF.evercloud.port)
+        self.auth_url = '/'.join((self.host, 'login'))
+        self.query_url = '/'.join((self.host, 'api', 'instances', 'vdi'))
+        self.action_url = '/'.join((self.host, 'api', 'instahces', 'vdi_action'))
         self.client = requests.session()
-        self.client.get(auth_url, verify = False) 
+        self.client.get(self.auth_url, verify = False) 
         csrftoken = client.cookies['csrftoken']
         login_data = {
             'username': username,
@@ -89,7 +92,7 @@ class Session(object):
             'csrfmiddlewaretoken': csrftoken,
             'next': '/'
         }
-        login_return = client.post(auth_url, data=login_data, headers={ 'Referer' = auth_url })
+        login_return = client.post(self.auth_url, data=login_data, headers={ 'Referer' = self.auth_url })
         if login_return.status_code == 200:
             self.token = csrftoken
             self.username = username
@@ -102,11 +105,10 @@ class Session(object):
 
         返回每个VM的id，状态和浮动ip。
         """
-        instances = self.client.get("http://{}:{}/api/instances/vdi/".format(CONF.evercloud.host, CONF.evercloud.port))
+        instances = self.client.get(self.query_url)
         info = {}
         for vmid in instances:
-            vm = instances[vmid]
-            vm = dict2obj(vm)
+            vm = dict2obj(instances[vmid])
             info[vm.vm_uuid] = {
                 'internal_id': vmid,
                 'name':        vm.name,
@@ -146,24 +148,29 @@ class Session(object):
         返回时VM已启动，或因错误无法启动，或操作超时。
         后两者抛出VMError异常。
         """
-        vm = self.get_vms()[vm_id]
-        if vm['status'] == 'SHUTOFF': # 只在关机状态下执行
+        vm = dict2obj(self.get_vms()[vm_id])
+        if vm.status == 'SHUTOFF': # 只在关机状态下执行
             log.info('Starting VM {}'.format(vm_id))
-            # TODO: adapt to evercloud
-            body = {'os-start': ''}
-            vm.action(session, body)
+            action = { 'instance': vm.internal_id, 'action': 'power_on' }
+            ret = self.client.get(self.action_url, params=action)
+            ret = json.loads(ret.content)
+            if ret['OPERATION_STATUS'] == 1: # success
+                self.wait_for_status(vm, 'ACTIVE', self.status_wait_timeout)
+                log.info('VM {} powered on'.format(vm_id))
 
-            self.wait_for_status(vm, 'ACTIVE', self.status_wait_timeout)
-            log.info('VM {} powered on'.format(vm_id))
-
-        info = {
-            vm_id: {
-                'status': 'ACTIVE',
-                'vnc_port': 123, # TODO
-                'spice_port': 123+1 # TODO
-            }
-        }
-        return info
+                vm = dict2obj(self.get_vms()[vm_id])
+                info = {
+                    vm_id: {
+                        'status': 'ACTIVE',
+                        'vnc_port': vm.vnc_port,
+                        'spice_port': vm.vnc_port + 1
+                    }
+                }
+                return info
+            else:
+                errmsg = 'Failed to start vm {}'.format(vm_id)
+                log.warning(errmsg)
+                raise VMError(errmsg)
 
     def stop_vm(self, vm_id):
         """关闭用户的VM。
@@ -171,13 +178,17 @@ class Session(object):
         返回时VM已关闭，或因错误无法关闭，或操作超时。
         后两者抛出VMError异常。
         """
-        vm = self.get_vms()[vm_id]
-        if vm['status'] == 'ACTIVE': # 只在开机状态下执行
+        vm = dict2obj(self.get_vms()[vm_id])
+        if vm.status == 'ACTIVE': # 只在开机状态下执行
             log.info('Shuting down VM {}'.format(vm_id))
-            # TODO: adapt to evercloud
-            body = {'os-stop': ''}
-            vm.action(session, body)
-
-            self.wait_for_status(vm, 'SHUTOFF', self.status_wait_timeout)
-            log.info('VM {} powered off'.format(vm_id))
+            action = { 'instance': vm.internal_id, 'action': 'power_off' }
+            ret = self.client.get(self.action_url, params=action)
+            ret = json.loads(ret.content)
+            if ret['OPERATION_STATUS'] == 1: # success
+                self.wait_for_status(vm, 'SHUTOFF', self.status_wait_timeout)
+                log.info('VM {} powered off'.format(vm_id))
+            else:
+                errmsg = 'Failed to stop vm {}'.format(vm_id)
+                log.warning(errmsg)
+                raise VMError(errmsg)
 
